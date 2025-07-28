1c78dfba84016fd7ef1a30c0069745b86a5bdb23
Nataliia74 <volkov.nataliia@gmail.com>
2025-07-28 09:36:54 +0100

    some styling

--- script.js
+++ script.js
@@ -3,7 +3,13 @@
 //const { createElement } = require("react");
 
 const rootElem = document.querySelector("#root");
-console.log(rootElem);
+//console.log(rootElem);
+
+const episode = getOneEpisode();
+
+// const episodeCard = document.createElement("div");
+// episodeCard.classList.add("episode_card");
+// rootElem.appendChild(episodeCard);
 
 function createEpisodeCardElement(
   parentElement,
@@ -21,41 +27,62 @@ function createEpisodeCardElement(
   return element;
 }
 
-const episodeCard = document.createElement("div");
-episodeCard.classList.add("episode_card");
-rootElem.appendChild(episodeCard);
-console.log(episodeCard);
+// createEpisodeCardElement(episodeCard, "h2", episode.name);
+// //console.log("h2");
 
-const episode = getOneEpisode();
+// createEpisodeCardElement(
+//   episodeCard,
+//   "p",
+//   `Episode code:  S${episode.season
+//     .toString()
+//     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`
+// );
+// //console.log("p");
 
-createEpisodeCardElement(episodeCard, "h2", episode.name);
-console.log("h2");
+// const imageEpisode = document.createElement("img");
+// imageEpisode.src = episode.image.medium;
+// imageEpisode.alt = episode.name;
+// episodeCard.appendChild(imageEpisode);
 
-createEpisodeCardElement(
-  episodeCard,
-  "p",
-  `Episode code:  S${episode.season
-    .toString()
-    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`
-);
-console.log("p");
-
-const imageEpisode = document.createElement("img");
-imageEpisode.src = episode.image.medium;
-imageEpisode.alt = episode.name;
-episodeCard.appendChild(imageEpisode);
-
-createEpisodeCardElement(episodeCard, "p", `Summary: ${episode.summary}`, true);
-console.log("p");
+// createEpisodeCardElement(episodeCard, "p", `Summary: ${episode.summary}`, true);
+// //console.log("p");
 
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(listEpisodes) {
-  const rootElem = document.getElementById("root");
-  listEpisodes.forEach(episode, () => {});
+function makePageForEpisodes(allEpisodes) {
+  allEpisodes.forEach((episode) => {
+    const episodeCard = document.createElement("div");
+    episodeCard.classList.add("episode_card");
+    rootElem.appendChild(episodeCard);
+    createEpisodeCardElement(
+      episodeCard,
+      "h2",
+      `${episode.name}  -  S${episode.season
+        .toString()
+        .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`
+    );
+    //console.log("h2");
+
+    // createEpisodeCardElement(
+    //   episodeCard,
+    //   "p",
+    //   `Episode code:  S${episode.season
+    //     .toString()
+    //     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`
+    // );
+    //console.log("p");
+
+    const imageEpisode = document.createElement("img");
+    imageEpisode.src = episode.image.medium;
+    imageEpisode.alt = episode.name;
+    episodeCard.appendChild(imageEpisode);
+
+    createEpisodeCardElement(episodeCard, "p", episode.summary, true);
+    //console.log("p");
+  });
 }
 
 window.onload = setup;
