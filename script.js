e60b9fbf58748d4a4892d388da447c935a27a227
Nataliia74 <volkov.nataliia@gmail.com>
2025-08-04 17:34:49 +0100

    refactoring partners code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //You can edit ALL of the code here
 
-const navBar = document.createElement("nav");
+const navBar = document.createElement("div");
 navBar.id = "header";
 document.body.insertBefore(navBar, document.body.firstChild);
 
@@ -37,7 +37,7 @@ function createEpisodeCardElement(
 }
 
 const allEpisodes = getAllEpisodes();
-const totalEpisodeQuantity = allEpisodes.length;
+const total = allEpisodes.length;
 
 function setup() {
   makePageForEpisodes(allEpisodes);
@@ -66,72 +66,91 @@ function makePageForEpisodes(allEpisodes) {
   });
 }
 
-
 // search bar part
 const episodeSelectDom = document.getElementById("episodes_selection");
-const selectAllOptionDom = document.createElement("option");
+navBar.appendChild(episodeSelectDom);
+const selectPlaceholderOptionDom = document.createElement("option");
+selectPlaceholderOptionDom.value = "";
+selectPlaceholderOptionDom.textContent = "Select an episode";
+selectPlaceholderOptionDom.disabled = true;
+selectPlaceholderOptionDom.selected = true;
+selectPlaceholderOptionDom.hidden = false;
+episodeSelectDom.appendChild(selectPlaceholderOptionDom);
 // this first select option to select all episodes
 // so it created here manually
+const selectAllOptionDom = document.createElement("option");
 selectAllOptionDom.value = "Select All Episodes";
-selectAllOptionDom.textContent = selectAllOptionDom.value;
+selectAllOptionDom.textContent = "Select All Episodes";
 episodeSelectDom.appendChild(selectAllOptionDom);
 
 function addSelectEntry(episode) {
   const optionDom = document.createElement("option");
-  optionDom.value = `S${episode.season
-    .toString()
-    .padStart(2, "0")
-    }E${episode.number.toString().padStart(2, "0")}` + " - " + episode.name;
+  optionDom.value =
+    `S${episode.season.toString().padStart(2, "0")}E${episode.number
+      .toString()
+      .padStart(2, "0")}` +
+    " - " +
+    episode.name;
   optionDom.textContent = optionDom.value;
   episodeSelectDom.appendChild(optionDom);
 }
 
-function modifyEpisodesQuantityDom(selected, total) {
-  const episodesQuantityDom = document.getElementById("display_quantity_dom");
-  let string = episodesQuantityDom.textContent;
-  string = string.replace("selected", String(selected));
-  string = string.replace("total", String(total));
-  episodesQuantityDom.textContent = string;
-}
-
 const selectOptionDom = document.getElementById("episodes_selection");
 selectOptionDom.addEventListener("change", function () {
-  let selectEpisodeName = selectOptionDom.value.substring(selectOptionDom.value.indexOf("- ") + 2);
-  if (selectOptionDom.value == "Select All Episodes") {
-    rootElem.innerHTML = '';
+  let selectEpisodeName = selectOptionDom.value.substring(
+    selectOptionDom.value.indexOf("- ") + 2
+  );
+  let selectedEpisodes = [];
+  if (selectOptionDom.value === "Select All Episodes") {
+    modifyEpisodesQuantityDom(allEpisodes.length, total);
+    rootElem.innerHTML = "";
     // recreate card for single selected
     makePageForEpisodes(allEpisodes);
   } else {
     // function makePageForEpisodes needs an array as argument
     // even if it is single element ( maybe refactor this in future)
-    let selectedEpisodes = [];
     for (const episode of allEpisodes) {
       if (episode.name == selectEpisodeName) {
         selectedEpisodes[0] = episode;
       }
     }
+
+    const selected = selectedEpisodes.length;
+    modifyEpisodesQuantityDom(selected, total);
     // clear all episodes
-    rootElem.innerHTML = '';
+    rootElem.innerHTML = "";
     // recreate card for single selected
     makePageForEpisodes(selectedEpisodes);
   }
 });
 
 const searchInputDom = document.getElementById("episode_input");
+navBar.appendChild(searchInputDom);
 searchInputDom.addEventListener("input", function () {
-  const search = searchInputDom.value;
+  const search = searchInputDom.value.toLowerCase();
   let selectedEpisodes = [];
   for (const episode of allEpisodes) {
-    if (episode.name.includes(search) || episode.summary.includes(search)) {
+    if (
+      episode.name.toLowerCase().includes(search) ||
+      episode.summary.toLowerCase().includes(search)
+    ) {
       selectedEpisodes.push(episode);
     }
   }
-  rootElem.innerHTML = '';
+  const selected = selectedEpisodes.length;
+  modifyEpisodesQuantityDom(selected, total);
+
+  rootElem.innerHTML = "";
   makePageForEpisodes(selectedEpisodes);
 });
 
-modifyEpisodesQuantityDom(0, totalEpisodeQuantity);
+const episodesQuantityDom = document.getElementById("display_quantity_dom");
+navBar.appendChild(episodesQuantityDom);
 
+function modifyEpisodesQuantityDom(selected, total) {
+  episodesQuantityDom.textContent = `Displaying ${selected}/${total} episodes.`;
+}
 
+modifyEpisodesQuantityDom(allEpisodes.length, total);
 
 window.onload = setup;
